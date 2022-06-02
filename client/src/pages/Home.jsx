dd5d49713675489b1f76700949b66a053dcb72b3
Samuel Ng <ngsamu4@gmail.com>
2022-06-02 17:39:12 -0400

    Basic routes.

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <>
+      <div>Welcome to Bridge</div>
+      <div>A Hub Dedicated to Connecting With Others </div>
+    </>
+  );
+};
+
+export default Home;

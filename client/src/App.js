ab3eaf0532dcb9ff4d386b9b6bc24127c4261a38
Samuel Ng <ngsamu4@gmail.com>
2022-06-26 18:53:04 -0400

    Fixed CRA .env variables.

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,18 @@ import ReactDom from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import socketClient from 'socket.io-client';
 
-import 'dotenv/config';
-
 import './App.css';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
 
 function App() {
+  console.log(
+    `${process.env.REACT_APP_SERVER_SCHEME}${process.env.REACT_APP_SERVER_DOMAIN}:${process.env.REACT_APP_SERVER_PORT}`
+  );
+  console.log('test');
   const socket = socketClient(
-    `${process.env.SERVER_SCHEME}${process.env.SERVER_DOMAIN}:${process.env.SERVER_PORT}`
+    `${process.env.REACT_APP_SERVER_SCHEME}${process.env.REACT_APP_SERVER_DOMAIN}:${process.env.REACT_APP_SERVER_PORT}`
   );
 
   socket.on('connection', () => {
